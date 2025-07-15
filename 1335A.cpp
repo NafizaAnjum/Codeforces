773b137e03beba471a9ed4a4680250623d2a25d2
NafizaAnjum <142255454+NafizaAnjum@users.noreply.github.com>
2025-07-15 07:04:00 +0600

    Add files via upload

---
+++ 1335A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    int n,w;
+
+    while(t--){
+        cin>>n;
+        w=(n-1)/2;
+        cout<<w<<endl;
+    }
+    return 0;
+}
